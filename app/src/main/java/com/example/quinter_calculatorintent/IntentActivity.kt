206fdbe31ca2f0ba7f3ea612a95a462838271d5f
JardinEgwa <egwajardin@gmail.com>
2022-07-19 12:12:52 +0300

    new

--- app/src/main/java/com/example/quinter_calculatorintent/IntentActivity.kt
+++ app/src/main/java/com/example/quinter_calculatorintent/IntentActivity.kt
@@ -1,11 +1,15 @@
 package com.example.quinter_calculatorintent
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 lateinit var Buttonsms:Button
@@ -14,6 +18,7 @@ lateinit var Buttoncamera:Button
 lateinit var Buttonemail:Button
 lateinit var ButtonMpesa:Button
 lateinit var Buttonshare:Button
+lateinit var Buttondial:Button
 
 class IntentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +30,7 @@ class IntentActivity : AppCompatActivity() {
         Buttonemail=findViewById(R.id.Btn_Email)
         ButtonMpesa=findViewById(R.id.Btn_STK)
         Buttonshare=findViewById(R.id.Btn_Share)
+        Buttondial=findViewById(R.id.Btn_Dial)
 
         Buttonsms.setOnClickListener {
             val uri = Uri.parse("smsto:0794838266")
@@ -61,10 +67,26 @@ class IntentActivity : AppCompatActivity() {
 
         }
         Buttoncall.setOnClickListener {
+            val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "0794838266"))
+            if (ContextCompat.checkSelfPermission(
+                    this@IntentActivity,
+                    Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this@IntentActivity,
+                    arrayOf(Manifest.permission.CALL_PHONE),
+                    1
+                )
+            } else {
+                startActivity(intent)
+            }
+
+        }
+        Buttondial.setOnClickListener {
             val phone = "0794838266"
             val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null))
             startActivity(intent)
-
         }
         
     }
